7c22aedca3e448ddd6fe30a22cbebe35f1f6170a
GURU909 <edgatto@ElGatto.University.liberty.edu>
2022-05-30 13:31:07 -0400

    - Removed Player Totem item as it is not used anymore - Updated mod Author, Description, Mod Credits, and Mod Logo.

--- src/main/java/net/mcreator/snookcraft/SnookcraftMod.java
+++ src/main/java/net/mcreator/snookcraft/SnookcraftMod.java
@@ -26,8 +26,6 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.FriendlyByteBuf;
 
-import net.mcreator.snookcraft.init.SnookcraftModItems;
-
 import java.util.function.Supplier;
 import java.util.function.Function;
 import java.util.function.BiConsumer;
@@ -44,8 +42,6 @@ public class SnookcraftMod {
 	public SnookcraftMod() {
 
 		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-
-		SnookcraftModItems.REGISTRY.register(bus);
 
 	}
 
