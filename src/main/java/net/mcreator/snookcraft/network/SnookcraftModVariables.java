cc306e728b0e89411a5fab9ae10072fae8f5433b
GURU909 <edgatto@ElGatto.University.liberty.edu>
2022-05-30 21:07:59 -0400

    - Temporarily removed chest checking. This will be a WIP. - Reloaded assets due to unexpected file corruption.

---
+++ src/main/java/net/mcreator/snookcraft/network/SnookcraftModVariables.java
@@ -0,0 +1,8 @@
+package net.mcreator.snookcraft.network;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class SnookcraftModVariables {
+
+}
